5995c7616941be527582bfa73de51ccc24f78adc
ifkimb <kcarolinw0704@gmail.com>
2025-09-15 22:08:01 -0300

    first commit

---
+++ fredly/src/main/java/com/fredly/fredly/FredlyApplication.java
@@ -0,0 +1,13 @@
+package com.fredly.fredly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FredlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FredlyApplication.class, args);
+	}
+
+}
