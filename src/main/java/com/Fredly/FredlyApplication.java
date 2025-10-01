917d50915585b557b62277473a9eda4cc99564be
ifkimb <kimberlycaroline900@gmail.com>
2025-10-01 19:55:45 -0300

    first commit

---
+++ src/main/java/com/Fredly/FredlyApplication.java
@@ -0,0 +1,13 @@
+package com.Fredly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FredlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FredlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Fredly/FredlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Fredly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FredlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
