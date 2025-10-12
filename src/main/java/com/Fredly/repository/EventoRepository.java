f1b3d7387dca8feac238742944d204a0477283c3
ifkimb <kcarolinw0704@gmail.com>
2025-10-11 21:30:46 -0300

    feat: implementa persistência do formulário de evento no bd

---
+++ src/main/java/com/Fredly/repository/EventoRepository.java
@@ -0,0 +1,9 @@
+package com.Fredly.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.Fredly.model.Evento;
+
+public interface EventoRepository extends CrudRepository<Evento, Long> {
+
+}
