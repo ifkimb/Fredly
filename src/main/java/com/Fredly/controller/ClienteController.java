0257ef5bf541c1e41b2f4c841f868c2975d3bc97
ifkimb <kcarolinw0704@gmail.com>
2025-11-07 22:48:30 -0300

    feat: adicionando exclusão de eventos

--- src/main/java/com/Fredly/controller/ClienteController.java
+++ src/main/java/com/Fredly/controller/ClienteController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.Fredly.model.Cliente;
@@ -35,6 +36,12 @@ public class ClienteController {
         List<Cliente> clientes = (List<Cliente>) er.findAll();
         model.addAttribute("clientes", clientes);
         return "listaClientes"; 
+    }
+
+     @PostMapping("/deletarCliente/{id_cliente}")
+    public String deletarCliente(@PathVariable Long id_cliente) {
+        er.deleteById(id_cliente);
+        return "redirect:/listaClientes";
     }
     
 }
