9165197326e53cfcb69568aea698d80fddba7abe
ifkimb <kcarolinw0704@gmail.com>
2025-11-07 23:06:16 -0300

    feat: adicionando edição de clientes

--- src/main/java/com/Fredly/controller/ClienteController.java
+++ src/main/java/com/Fredly/controller/ClienteController.java
@@ -6,17 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.Fredly.model.Cliente;
+import com.Fredly.model.Evento;
 import com.Fredly.repository.ClienteRepository;
 
 @Controller
 public class ClienteController {
     
     @Autowired
-    private ClienteRepository er;
+    private ClienteRepository cr;
 
     @GetMapping("/cadastrarCliente")
     public String formCliente() {
@@ -26,21 +28,46 @@ public class ClienteController {
 
     @PostMapping("/cadastrarCliente")
     public String salvarCliente(Cliente cliente) {
-        er.save(cliente);
+        cr.save(cliente);
         return "redirect:/listaClientes";
     }
 
 
      @GetMapping("/listaClientes")
     public String inicio(Model model) {
-        List<Cliente> clientes = (List<Cliente>) er.findAll();
+        List<Cliente> clientes = (List<Cliente>) cr.findAll();
         model.addAttribute("clientes", clientes);
         return "listaClientes"; 
     }
 
      @PostMapping("/deletarCliente/{id_cliente}")
     public String deletarCliente(@PathVariable Long id_cliente) {
-        er.deleteById(id_cliente);
+        cr.deleteById(id_cliente);
+        return "redirect:/listaClientes";
+    }
+
+    @GetMapping("/editarCliente/{id_cliente}")
+    public String editarCliente(@PathVariable Long id_cliente, Model model) {
+        Cliente cliente = cr.findById(id_cliente)
+                .orElseThrow(() -> new IllegalArgumentException("Cliente inválido: " + id_cliente));
+        model.addAttribute("cliente", cliente);
+        return "editarCliente";
+    }
+
+    @PostMapping("/editarCliente/{id_cliente}")
+    public String atualizarCliente(@PathVariable Long id_cliente, @ModelAttribute Cliente clienteAtualizado) {
+        Cliente cliente = cr.findById(id_cliente)
+                .orElseThrow(() -> new IllegalArgumentException("Cliente inválido: " + id_cliente));
+
+        cliente.setNomecliente(clienteAtualizado.getNomecliente());
+        cliente.setEmail(clienteAtualizado.getEmail());
+        cliente.setTelefone(clienteAtualizado.getTelefone());
+        cliente.setStatus(clienteAtualizado.getStatus());
+        cliente.setObservacoes(clienteAtualizado.getObservacoes());
+
+        
+
+        cr.save(cliente);
         return "redirect:/listaClientes";
     }
     
--- src/main/java/com/Fredly/repository/ClienteRepository.java
+++ src/main/java/com/Fredly/repository/ClienteRepository.java
@@ -2,11 +2,11 @@ package com.Fredly.repository;
 
 import java.util.List;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.Fredly.model.Cliente;
 
-public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
     static List<Cliente> listarTodos() {
         throw new UnsupportedOperationException("Unimplemented method 'listarTodos'");
